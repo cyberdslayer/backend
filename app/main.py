e379c685067874c38b4becf2433c6c9a4da664c0
Shubham Mishra <meshuham@duck.com>
2025-01-07 22:22:08 +0530

    cors'

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ frontend_url = os.getenv("FRONTEND_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
